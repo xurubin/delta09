d7dbed2a8eae0b11dded44a284b0620b3da71a9c
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-02 13:52:28 +0000

    fixing clarifications from Rubin

--- src/org/delta/transport/CircuitTransport.java
+++ src/org/delta/transport/CircuitTransport.java
@@ -4,6 +4,7 @@ import org.delta.circuit.ComponentGraph;
 
 class CircuitTransport {
 	DatagramLayer datagramLayer;
+	public static int MAX_PAYLOAD = 56; 
 	
 	public CircuitTransport(DatagramLayer d) {
 		this.datagramLayer = d;
@@ -18,17 +19,15 @@ class CircuitTransport {
 				
 		int remainingBytes = byteArray.length;
 		int i = 0;
-		int j = 0;
-		
 		
 		while(remaningBytes > 0) {
 			//create 50 bytes long payload
-			byte[] datagramPayload = new byte[50];
-			//split array into chunks of length 50.
-			if(remainingBytes >= 50) {
-				System.arraycopy(byteArray, i, datagramPayload, 0, 50);
-				remainingBytes -= 50;
-				i += 50;
+			byte[] datagramPayload = new byte[MAX_PAYLOAD];
+			//split array into chunks of length MAX_PAYLOAD.
+			if(remainingBytes >= MAX_PAYLOAD) {
+				System.arraycopy(byteArray, i, datagramPayload, 0, MAX_PAYLOAD);
+				remainingBytes -= MAX_PAYLOAD;
+				i += MAX_PAYLOAD;
 			}
 			else {
 				System.arraycopy(byteArray, i, datagramPayload, 0, remainingBytes);
@@ -39,8 +38,6 @@ class CircuitTransport {
 			datagramLayer.sendDatagram(new Datagram(datagramPayload));
 		}
 		
-		//send datagrams
-		datagramLayer.handler();
 		
 	}
 	

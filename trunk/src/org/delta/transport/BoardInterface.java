7470043147566c705faf2236ef16dddf187ccfa8
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-03 22:37:09 +0000

    oops

--- trunk/src/org/delta/transport/BoardInterface.java
+++ trunk/src/org/delta/transport/BoardInterface.java
@@ -10,7 +10,7 @@ class BoardInterface {
 	public BoardInterface(Simulator simulator) {
 		//start listener
 		SerialListener s = new SerialListener(usbInterface, simulator);
-		s.run();
+		s.start();
 		
 	}
 	

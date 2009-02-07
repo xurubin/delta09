50d4bb96c785da1e97497a3b8c1f8023fe850f89
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-07 15:31:37 +0000

    Reworked transports using Rubin's new methods. Adding VerilogConverter

--- trunk/src/org/delta/transport/BoardInterface.java
+++ trunk/src/org/delta/transport/BoardInterface.java
@@ -1,14 +1,24 @@
 package org.delta.transport;
-import org.delta.simulation.SimulationEvent;
 import org.delta.simulation.Simulator;
-import com.jopdesign.io.HostDatagramLayer;
-import com.jopdesign.io.Packet;
+//import com.jopdesign.io.HostDatagramLayer;
+//import com.jopdesign.io.Packet;
 
+
+/**
+ * Interface to USB serial link to DE2 board. 
+ * Provides method to send LED changes to board. 
+ * @author Group Delta 2009
+ *
+ */
 class BoardInterface {
 	
-	private volatile byte[] lights;
+	private volatile int lights = 0;
 	private HostDatagramLayer hostLayer;
 	
+	/**
+	 * Constructs a new BoardInterface.
+	 * @param simulator	a reference to the simulator object, so call-backs can be used.
+	 */
 	public BoardInterface(Simulator simulator) {
 		//start listener
 		HostDatagramLayer hostLayer  = new HostDatagramLayer(1);
@@ -16,11 +26,17 @@ class BoardInterface {
 		s.start();
 		
 	}
-	
-	public sendEvent(SimulationEvent event) {
-		//compare state change event to lights boolean array.
-		//send state change packet. 
-		Packet p = new Packet();
-		hostLayer.sendDatagram(p);
+	/**
+	 * Sends a packet to the DE2 board with the new status of the lights.
+	 * @param i	the number of the LED on the board.
+	 * @param b	the value to set that LED to.
+	 * @return 1 if successful. 
+	 */
+	public int sendLEDEvent(int i, boolean b) {
+		//compare state change event to lights packed int.
+		int status = (b ? 1 : 0) << (31 - i);
+		status |= lights;
+		//send LED state packet. 
+		return hostLayer.sendLEDStates(status);
 	}
 }

686a6a29f03916baa890888761ee0b5687f6f8f5
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-08 17:48:51 +0000

    tidying up.

--- trunk/src/org/delta/circuit/Wire.java
+++ trunk/src/org/delta/circuit/Wire.java
@@ -4,7 +4,8 @@ import org.delta.logic.State;
 import org.jgrapht.graph.DefaultEdge;
 
 public class Wire extends DefaultEdge {
-    private State state = State.SX;
+	private static final long serialVersionUID = 1L;
+	private State state = State.SX;
     
     public State getState() {
         return state;

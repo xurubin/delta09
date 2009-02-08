2f2c86395c40d5987f83c94e0fb8deb0575e4ba3
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-08 15:47:57 +0000

    Added some file handling to VerilogConverter.

--- trunk/src/org/delta/circuit/gate/NorGate.java
+++ trunk/src/org/delta/circuit/gate/NorGate.java
@@ -16,6 +16,7 @@ public class NorGate extends Gate {
         return new Nor(new Constant(getWire(0)), new Constant(getWire(1)));
     }
     
+    @Override
     public String getVerilogGateName() {
     	return "nor";
     }

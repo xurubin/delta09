0a331de964328accea8ba4edfa41a455cf9b68b7
davidjweston <davidjweston@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-14 11:02:40 +0000

    Testing collapsible menu

--- trunk/src/org/delta/gui/ClockPanel.java
+++ trunk/src/org/delta/gui/ClockPanel.java
@@ -1,6 +1,7 @@
 package org.delta.gui;
 
 import javax.swing.*;
+
 import java.awt.*;
 
 public class ClockPanel extends javax.swing.JPanel
@@ -19,14 +20,21 @@ public class ClockPanel extends javax.swing.JPanel
 		
 		clock_image = il.loadImage ("clock.png");
 		
+		ImageIcon clock_icon = new ImageIcon ("src/org/delta/gui/diagram/images/clock.png");
+		
+		JLabel clock_label = new JLabel(clock_icon);
+		
+		setLayout (new GridLayout (1, 2) );
+		
 		spinner = new JSpinner ( new SpinnerNumberModel (50, 1, 100, 1) );
 		
-		add (spinner, BorderLayout.CENTER);
+		add (clock_label);
+		add (spinner);
 	}
 	
 	public void paintComponent (Graphics g)
 	{
 		super.paintComponent (g);
-		g.drawImage (clock_image, 20, 20, this);
+//		g.drawImage (clock_image, 20, 20, this);
 	}
 }

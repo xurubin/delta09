d9ac0987886b7e27b08976a0e5a7f0391509643f
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-15 20:47:05 +0000

    Adding stop action

---
+++ src/org/delta/gui/StopAction.java
@@ -0,0 +1,23 @@
+package org.delta.gui;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class StopAction extends AbstractAction
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StopAction(String text, ImageIcon icon, String accelerator)
+	{
+		super(text);
+		this.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(accelerator));
+		this.putValue(Action.LARGE_ICON_KEY, icon);
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		// Stop simulation
+	}
+}

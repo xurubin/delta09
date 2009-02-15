52a0310c6f469ae897be2b3e7cdbeba15bc7c1da
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-15 22:43:25 +0000

    Removing tooltips, because they also appear in each JMenuItem. This is extremely annoying.

--- src/org/delta/gui/SaveAction.java
+++ src/org/delta/gui/SaveAction.java
@@ -9,11 +9,12 @@ public class SaveAction extends AbstractAction
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public SaveAction(String text, ImageIcon icon, String accelerator)
+	public SaveAction(String text, ImageIcon icon, String accelerator,int mnemonic)
 	{
 		super(text);
 		this.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(accelerator));
 		this.putValue(Action.LARGE_ICON_KEY, icon);
+		this.putValue(Action.MNEMONIC_KEY, mnemonic);
 	}
 	
 	public void actionPerformed(ActionEvent e)

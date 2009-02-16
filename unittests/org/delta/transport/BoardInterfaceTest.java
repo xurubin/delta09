b32ce2202828a24b75a5684ba54d8b7baa129846
xurubin <xurubin@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-16 15:46:51 +0000

    Unit test for HostDatagramLayer. USB_UnitTest.jop must be running on DE2.

---
+++ unittests/org/delta/transport/BoardInterfaceTest.java
@@ -0,0 +1,39 @@
+package org.delta.transport;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BoardInterfaceTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSendLEDEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSwitchStatus() {
+		fail("Not yet implemented");
+	}
+
+}

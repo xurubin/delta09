e654af8b2d3a1c77f0c8ce0d204511d79c65fc7f
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-16 17:27:14 +0000

    Modifying classpath.

--- test/org/delta/gui/i18n/TestTranslator.java
+++ test/org/delta/gui/i18n/TestTranslator.java
@@ -2,16 +2,16 @@ package org.delta.gui.i18n;
 
 
 import java.util.Locale;
+
+import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import org.delta.gui.MainWindow;
 
 public class TestTranslator {
 	public static void main(String args[]) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-		//UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		//MainWindow mw = new MainWindow(new Locale("ja", "JP"));
-		//MainWindow mw = new MainWindow(new Locale("zh", "CN"));
-		MainWindow mw = new MainWindow(new Locale("fr", "FR"));
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		MainWindow mw = new MainWindow(new Locale("fr"));
 		mw.setVisible(true);
 	}
 }

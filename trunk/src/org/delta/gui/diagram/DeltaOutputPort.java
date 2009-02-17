8f7c15c3b4405389749d55a8d75e73afba4d463e
cwilso99@hotmail.com <cwilso99@hotmail.com@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-17 14:42:01 +0000

    Added numbers to input and output ports with the accessor method getPortNumber().

--- trunk/src/org/delta/gui/diagram/DeltaOutputPort.java
+++ trunk/src/org/delta/gui/diagram/DeltaOutputPort.java
@@ -2,16 +2,34 @@ package org.delta.gui.diagram;
 
 import org.jgraph.graph.DefaultPort;
 
-public class DeltaOutputPort extends DefaultPort
-{
-	/**
-	 * 
-	 */
+/**
+ * Subclass of DefaultPort that represents outputs from a component.
+ * @author Group Delta 2009
+ */
+public class DeltaOutputPort extends DefaultPort {
+	/** Needed for correct serialization. */
 	private static final long serialVersionUID = 1L;
-
-	public DeltaOutputPort()
-	{
+	
+	/**
+	 * Number of port with respect to the component it is attached to.
+	 * Used for translation to the simulation graph.
+	 */
+	private int portNumber;
+	
+	/**
+	 * Create a DeltaInputPort with the given portNumber.
+	 * @param number - the number of this port with respect to its component.
+	 */
+	public DeltaOutputPort(int number) {
 		super();
-		
+		portNumber = number;
+	}
+	
+	/**
+	 * Accessor method for portNumber.
+	 * @return The number of this port with respect to it's component.
+	 */
+	public int getPortNumber() {
+		return portNumber;
 	}
 }

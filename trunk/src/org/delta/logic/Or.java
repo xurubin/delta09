bd47eb3a6b785eb16d5cae8123b13e56f1728510
justus.matthiesen <justus.matthiesen@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-17 02:09:44 +0000

    Adds abstract BinaryFunction class so we can use a factory to create new gates from these functions.

--- trunk/src/org/delta/logic/Or.java
+++ trunk/src/org/delta/logic/Or.java
@@ -1,23 +1,14 @@
 package org.delta.logic;
 
-public class Or extends Formula {
-    private Formula arg0 = new Constant(State.SX);
-    private Formula arg1 = new Constant(State.SX);
-
-    public Or(Formula arg0, Formula arg1) {
-        if (arg0 != null)
-            this.arg0 = arg0;
-        if (arg1 != null)
-            this.arg1 = arg1;
-    }
+public class Or extends BinaryFunction {
 
     @Override
     public State evaluate() {
-        switch (arg0.evaluate()) {
+        switch (evaluateArg0()) {
         case S1: return State.S1;
-        case SX: return (arg1.evaluate() == State.S1)? State.S1 : State.SX;
+        case SX: return (evaluateArg1() == State.S1)? State.S1 : State.SX;
         case S0:
-        default: return arg1.evaluate();
+        default: return evaluateArg1();
         }
     }
 
--- trunk/src/org/delta/logic/Xor.java
+++ trunk/src/org/delta/logic/Xor.java
@@ -1,23 +1,14 @@
 package org.delta.logic;
 
-public class Xor extends Formula {
-    private Formula arg0 = new Constant(State.SX);
-    private Formula arg1 = new Constant(State.SX);
-
-    public Xor(Formula arg0, Formula arg1) {
-        if (arg0 != null)
-            this.arg0 = arg0;
-        if (arg1 != null)
-            this.arg1 = arg1;
-    }
+public class Xor extends BinaryFunction {
 
     @Override
     public State evaluate() {
-        switch (arg0.evaluate()) {
-        case S0: return arg1.evaluate();
+        switch (evaluateArg0()) {
+        case S0: return evaluateArg1();
         case SX: return State.SX;
         case S1:
-        default: return (new Not(arg1)).evaluate();
+        default: return (new Not(getArg1())).evaluate();
         }
     }
 

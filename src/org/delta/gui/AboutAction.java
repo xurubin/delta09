030bd0082323c41005bf624fa88ea0bd485faedd
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-19 16:09:50 +0000

    Added about menu item.

--- src/org/delta/gui/AboutAction.java
+++ src/org/delta/gui/AboutAction.java
@@ -1,13 +1,16 @@
 package org.delta.gui;
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
-import java.net.URL;
 
-import javax.help.HelpSet;
-import javax.help.JHelp;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
-import javax.swing.JDialog;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.KeyStroke;
 
 public class AboutAction extends AbstractAction
@@ -27,7 +30,21 @@ public class AboutAction extends AbstractAction
 	public void actionPerformed(ActionEvent e)
 	{
 		// Display about dialog.
+		JFrame jf = new JFrame();
+		Container container = jf.getContentPane();
+		container.setLayout(new BorderLayout());
+		container.setPreferredSize(new Dimension(500,400));
 		
+		ImageIcon icon = new ImageIcon("src/org/delta/gui/icons/logo.png");
+		JLabel imageLabel = new JLabel("", icon, JLabel.CENTER);
+		JLabel text = new JLabel("<html><center><h1>Project Delta: An Interactive FPGA Circuit Simulator</h1><p>Developed for CST IB group project. </p><h2>Developed By:</h2>" +
+				"<p>Robert Duncan<br>Justus Matthiesen<br>David Weston<br>Christopher Wilson<br>Rubin Xu</p></center></html>", JLabel.CENTER);
+		
+		container.add(imageLabel, BorderLayout.NORTH);
+		container.add(text, BorderLayout.CENTER);
+		
+		jf.pack();
+		jf.setVisible(true);
 		
 	}
 }

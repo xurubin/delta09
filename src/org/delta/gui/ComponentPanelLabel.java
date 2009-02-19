f96264603198941ddcd625fc61e6d8b420fcdadc
davidjweston <davidjweston@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-19 16:47:47 +0000

    Added pretty mouseover features to component panel

--- src/org/delta/gui/ComponentPanelLabel.java
+++ src/org/delta/gui/ComponentPanelLabel.java
@@ -1,5 +1,9 @@
 package org.delta.gui;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 
@@ -11,11 +15,33 @@ public class ComponentPanelLabel extends JLabel {
 	private static final long serialVersionUID = 1L;
 	private int componentKey;
 	
+	private Font f1, f2;
+	
 	public ComponentPanelLabel(ImageIcon icon, String name, int key) {
 		super(name, icon, JLabel.CENTER);
 		setHorizontalTextPosition(JLabel.CENTER);
 		setVerticalTextPosition(JLabel.BOTTOM);
 		componentKey = key;
+		
+		f1 = getFont();
+		f2 = new Font ( f1.getName(), Font.BOLD, f1.getSize() );
+		
+		addMouseListener ( new MouseAdapter() {
+			public void mouseEntered (MouseEvent e)
+			{
+				setFont (f2);
+			}
+			
+			public void mouseExited (MouseEvent e)
+			{
+				setFont (f1);
+			}
+			
+			public void mousePressed (MouseEvent e)
+			{
+				setFont (f1);
+			}
+		} );
 	}
 	
 	public int getComponentKey() {

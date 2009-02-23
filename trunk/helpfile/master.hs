ceb970b585c1fd31c84650cd03f37d73c4dcfb83
xurubin <xurubin@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-23 00:32:08 +0000

    Help file structure generated.

---
+++ trunk/helpfile/master.hs
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<helpset>
+	<title>Delta Circuit Simulator</title>
+	<maps>
+		<mapref location="map.xml"/>
+	</maps>
+	<view>
+		<name>Table of Contents</name>
+		<label>Table of Contents</label>
+		<type>javax.help.TOCView</type>
+		<data>toc.xml</data>
+	</view>
+</helpset> 

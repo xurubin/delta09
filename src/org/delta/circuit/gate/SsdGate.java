daa7d88858b83e5558ad1b88fde5c65191ddd623
justus.matthiesen <justus.matthiesen@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-23 15:57:37 +0000

    Implements the logic side of the seven segment display.

---
+++ src/org/delta/circuit/gate/SsdGate.java
@@ -0,0 +1,25 @@
+package org.delta.circuit.gate;
+
+import org.delta.logic.Constant;
+import org.delta.logic.Formula;
+import org.delta.logic.Function;
+import org.delta.logic.SsdFunction;
+
+public class SsdGate extends AbstractOutputGate {
+    private static final long serialVersionUID = 1L;
+    private Function ssd = new SsdFunction();
+
+    public SsdGate() {
+        super(7);
+    }
+
+    @Override
+    public Formula getFormula() {
+        for (int i = 0; i < getInputCount(); ++i) {
+            ssd.setArgument(i, new Constant(getWire(i)));
+        }
+        
+        return ssd;
+    }
+
+}

0c1984c7b58b725a86036b343adb771263e81af4
justus.matthiesen <justus.matthiesen@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-23 16:18:55 +0000

    Adds methods to set the number of the seven segment display.

--- src/org/delta/circuit/gate/SsdGate.java
+++ src/org/delta/circuit/gate/SsdGate.java
@@ -2,19 +2,25 @@ package org.delta.circuit.gate;
 
 import org.delta.logic.Constant;
 import org.delta.logic.Formula;
-import org.delta.logic.Function;
 import org.delta.logic.SsdFunction;
 
 public class SsdGate extends AbstractOutputGate {
     private static final long serialVersionUID = 1L;
-    private Function ssd = new SsdFunction();
+    private SsdFunction ssd = new SsdFunction();
+    private int ssdNumber;
    
-    public SsdGate() {
+    public SsdGate(int ssdNumber) {
         super(7);
+        this.ssdNumber = ssdNumber;
+    }
+    
+    public void setSsdNumber(int ssdNumber) {
+        this.ssdNumber = ssdNumber;
     }
 
     @Override
     public Formula getFormula() {
+        ssd.setSsdNumber(ssdNumber);
         for (int i = 0; i < getInputCount(); ++i) {
             ssd.setArgument(i, new Constant(getWire(i)));
         }

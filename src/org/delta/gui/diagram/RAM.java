5ef25544a8e221a0a7905f2e07b3fbde6df87d05
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-23 17:13:50 +0000

    More RAM and ROM

---
+++ src/org/delta/gui/diagram/RAM.java
@@ -0,0 +1,58 @@
+package org.delta.gui.diagram;
+
+import java.awt.Point;
+import java.awt.geom.Rectangle2D;
+
+import org.delta.circuit.Component;
+import org.delta.circuit.component.DFlipFlopComponent;
+import org.jgraph.graph.GraphConstants;
+
+/**
+ * Class to represent the "model" of a DFlip (D flip flop) in the circuit diagram.
+ * @author Group Delta 2009
+ */
+public class RAM extends DeltaComponent {
+	/** Needed for correct serialization. */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Creates a new NotGate at a default position.
+	 */
+	public RAM() {
+		this(new Point(100,100));
+	}
+	
+	/**
+	 * Creates a new DFlip at the given position on the screen.
+	 * @param position - where to insert the new component.
+	 */
+	public RAM(Point position) {
+		super();
+		
+		this.replaceUserObject();
+		
+		this.addInputPort(new Point(0,120),0);
+		this.addInputPort(new Point(0,280),1);
+		this.addInputPort(new Point(0,430),2);
+		this.addInputPort(new Point(0,580),3);
+		this.addInputPort(new Point(0,790),4);
+		this.addInputPort(new Point(0,920),5);
+		this.addOutputPort(new Point(GraphConstants.PERMILLE,180),0);
+		// Set position based on parameter
+		Rectangle2D bounds = new Rectangle2D.Double(position.getX(),position.getY(),60,40);
+		GraphConstants.setBounds(this.getAttributes(),bounds);
+	}
+	
+	/**
+	 * Replaces the userObject with a new one. This is used when copying the cell so that
+	 * a new component is created on the simulation graph - otherwise we would have two
+	 * display graph components being represented by just one simulation graph component.
+	 */
+	protected void replaceUserObject() {
+		/*
+		 * TODO: add correct ROM component
+		 */
+        Component component = new DFlipFlopComponent();
+		this.setUserObject(component);
+	}
+}
---
+++ src/org/delta/gui/diagram/ROM.java
@@ -0,0 +1,56 @@
+package org.delta.gui.diagram;
+
+import java.awt.Point;
+import java.awt.geom.Rectangle2D;
+
+import org.delta.circuit.Component;
+import org.delta.circuit.component.DFlipFlopComponent;
+import org.jgraph.graph.GraphConstants;
+
+/**
+ * Class to represent the "model" of a DFlip (D flip flop) in the circuit diagram.
+ * @author Group Delta 2009
+ */
+public class ROM extends DeltaComponent {
+	/** Needed for correct serialization. */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Creates a new NotGate at a default position.
+	 */
+	public ROM() {
+		this(new Point(100,100));
+	}
+	
+	/**
+	 * Creates a new DFlip at the given position on the screen.
+	 * @param position - where to insert the new component.
+	 */
+	public ROM(Point position) {
+		super();
+		
+		this.replaceUserObject();
+		
+		this.addInputPort(new Point(0,180),0);
+		this.addInputPort(new Point(0,400),1);
+		this.addInputPort(new Point(0,610),2);
+		this.addInputPort(new Point(0,830),3);
+		this.addOutputPort(new Point(GraphConstants.PERMILLE,180),0);
+		// Set position based on parameter
+		Rectangle2D bounds = new Rectangle2D.Double(position.getX(),position.getY(),60,40);
+		GraphConstants.setBounds(this.getAttributes(),bounds);
+	}
+	
+	/**
+	 * Replaces the userObject with a new one. This is used when copying the cell so that
+	 * a new component is created on the simulation graph - otherwise we would have two
+	 * display graph components being represented by just one simulation graph component.
+	 */
+	protected void replaceUserObject() {
+		/*
+		 * TODO: add correct ROM component
+		 */
+        Component component = new DFlipFlopComponent();
+		this.setUserObject(component);
+	}
+}

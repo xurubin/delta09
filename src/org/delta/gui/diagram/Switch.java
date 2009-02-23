441c92c3f0ba2dfe57a32c2302c5868905b616a4
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-23 15:22:37 +0000

    Switch class.

---
+++ src/org/delta/gui/diagram/Switch.java
@@ -0,0 +1,52 @@
+package org.delta.gui.diagram;
+
+import java.awt.Point;
+import java.awt.geom.Rectangle2D;
+
+import org.delta.circuit.Component;
+import org.delta.circuit.Gate;
+import org.delta.circuit.component.GateComponentFactory;
+import org.delta.circuit.gate.SwitchGate;
+import org.jgraph.graph.GraphConstants;
+
+/**
+ * Class to represent the "model" of a constant high input in the circuit diagram.
+ * @author Group Delta 2009
+ */
+public class Switch extends DeltaComponent {
+	/** Needed for correct serialization. */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Creates a new Switch at a default position.
+	 */
+	public Switch() {
+		this(new Point(100,100));
+	}
+	
+	/**
+	 * Creates a new Switch at the given position on the screen.
+	 * @param position - where to insert the new component.
+	 */
+	public Switch(Point position) {
+		super();
+		
+		this.replaceUserObject();
+
+		this.addOutputPort(new Point(GraphConstants.PERMILLE,GraphConstants.PERMILLE / 2),0);
+		// Set position based on parameter
+		Rectangle2D bounds = new Rectangle2D.Double(position.getX(),position.getY(),60,40);
+		GraphConstants.setBounds(this.getAttributes(),bounds);
+	}
+	
+	/**
+	 * Replaces the userObject with a new one. This is used when copying the cell so that
+	 * a new component is created on the simulation graph - otherwise we would have two
+	 * display graph components being represented by just one simulation graph component.
+	 */
+	protected void replaceUserObject() {
+	    Gate gate = new SwitchGate(0);
+        Component component = GateComponentFactory.createComponent(gate);
+		this.setUserObject(component);
+	}
+}

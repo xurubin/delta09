7e2147e5ded3b88b28888864a9869893bada60a3
justus.matthiesen <justus.matthiesen@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-23 15:57:37 +0000

    Implements the logic side of the seven segment display.

---
+++ trunk/src/org/delta/logic/SsdFunction.java
@@ -0,0 +1,34 @@
+package org.delta.logic;
+
+import org.delta.circuit.Wire;
+import org.delta.transport.BoardInterface;
+
+public class SsdFunction extends Function {
+    private int ssdNumber;
+    
+    public SsdFunction() {
+        super(7);
+    }
+    
+    public void setSsdNumber(int ssdNumber) {
+        this.ssdNumber = ssdNumber;
+    }
+
+    @Override
+    public State evaluate() {
+        BoardInterface board = BoardInterface.getInstance(); 
+        
+        for (int i = 0; i < getArgumentCount(); ++i) {
+            State state = evaluateArgument(i); 
+            if (state == State.S0) {
+                board.sendHEXEvent(ssdNumber, i, false);
+            } else if (state == State.S1) {
+                board.sendHEXEvent(ssdNumber, i, false);
+            }
+        }
+        
+        // Return value does not matter as it will not be used.
+        return State.SX;
+    }
+
+}

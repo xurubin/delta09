1d6547d6b8c8cdac566bb9965503ea5590aa4cf1
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-24 18:01:44 +0000

    Got rid of test code

--- trunk/src/org/delta/verilog/VerilogConverter.java
+++ trunk/src/org/delta/verilog/VerilogConverter.java
@@ -20,19 +20,6 @@ import org.delta.circuit.ComponentGraph;
 import org.delta.circuit.ComponentWire;
 import org.delta.circuit.Gate;
 import org.delta.circuit.Wire;
-import org.delta.circuit.component.DFlipFlopComponent;
-import org.delta.circuit.component.ComponentFactory;
-import org.delta.circuit.gate.GateFactory;
-import org.delta.circuit.gate.HighGate;
-import org.delta.circuit.gate.LedGate;
-import org.delta.circuit.gate.SwitchGate;
-import org.delta.logic.And;
-import org.delta.logic.Or;
-
-
-/*
- * TODO: ADD HASHMAP passing of wire names! 
- */
 
 /**
  * Converts our data structure into Verilog.
@@ -292,58 +279,5 @@ public class VerilogConverter {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-
-	/**
-	 * Demo main function.
-	 * 
-	 * @param args
-	 *            command line arguments.
-	 */
-	public static void main(String[] args) {
-		// some simple demo code.
-		ComponentGraph c = new ComponentGraph();
-		
-		Component andGate = ComponentFactory.createComponent(GateFactory.createGate(And.class, 2));
-		Component orGate = ComponentFactory.createComponent(GateFactory.createGate(Or.class, 4));
-		Component dFlip = new DFlipFlopComponent();
-			
-		Component switchOne = ComponentFactory.createComponent(new SwitchGate(1));
-		Component highGate = ComponentFactory.createComponent(new HighGate());
-		Component ledOne = ComponentFactory.createComponent(new LedGate(1));
-		Component ledTwo = ComponentFactory.createComponent(new LedGate(2));
-		
-		c.addVertex(switchOne);
-		c.addVertex(highGate);
-		c.addVertex(andGate);
-		c.addVertex(orGate);
-		c.addVertex(ledOne);
-		c.addVertex(dFlip);
-		c.addVertex(ledTwo);
-		
-		ComponentWire w1 = c.addEdge(switchOne, andGate);
-		ComponentWire w2 = c.addEdge(highGate, andGate);
-		ComponentWire w3 = c.addEdge(andGate, orGate);
-		ComponentWire w4 = c.addEdge(switchOne, orGate);
-		ComponentWire w5 = c.addEdge(highGate, orGate);
-		ComponentWire w6 = c.addEdge(orGate, ledOne);
-		ComponentWire w7 = c.addEdge(orGate, dFlip);
-		ComponentWire w8 = c.addEdge(dFlip, orGate);
-		ComponentWire w9 = c.addEdge(dFlip, ledTwo);
-		
-		c.registerEdge(w1, 0, 0);
-        c.registerEdge(w2, 0, 1);
-        c.registerEdge(w3, 0, 0);
-		c.registerEdge(w4, 0, 1);
-        c.registerEdge(w5, 0, 2);
-        c.registerEdge(w6, 0, 0);
-        c.registerEdge(w7, 0, 0);
-        c.registerEdge(w8, 0, 3);
-        c.registerEdge(w9, 1, 0);
-        
-        
-		System.out.println(VerilogConverter.convertToVerilog(c));
-
-		
 	}
 }

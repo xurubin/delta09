4d63d7206af0690d62ff1d642e66949f0ff24a1f
cwilso99@hotmail.com <cwilso99@hotmail.com@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-25 18:56:43 +0000

    Fixes issue 6 by removing edges attached to a component that is cut.

--- src/org/delta/gui/CutAction.java
+++ src/org/delta/gui/CutAction.java
@@ -1,6 +1,16 @@
 package org.delta.gui;
 import java.awt.event.*;
+import java.util.Set;
+
 import javax.swing.*;
+
+import org.delta.gui.diagram.DeltaGraph;
+//import org.delta.gui.diagram.DeltaGraphModel;
+//import org.delta.gui.diagram.Ledg;
+//import org.delta.gui.diagram.Ledr;
+//import org.delta.gui.diagram.SevenSegment;
+import org.jgraph.graph.DefaultGraphModel;
+import org.jgraph.graph.Edge;
 
 
 public class CutAction extends AbstractAction
@@ -20,11 +30,58 @@ public class CutAction extends AbstractAction
 		this.putValue(Action.MNEMONIC_KEY, mnemonic);
 	}
 	
+	/**
+	 * Perform the "cut" action for the given event. This moves all cells
+	 * in the current selection to the clipboard (using the default Swing
+	 * action) and deletes all edges connected to them to ensure we are
+	 * not left with dangling edges.
+	 * @param e - event that fired the DeleteAction
+	 */
+	@SuppressWarnings("unchecked")
 	public void actionPerformed(ActionEvent e)
 	{
 		e = new ActionEvent(MainWindow.get().circuit_panel.getGraph(),
 				e.getID(),e.getActionCommand(),e.getModifiers());
+
+		// INSERTED
+		DeltaGraph graph = (DeltaGraph)MainWindow.get().circuit_panel.getGraph();
+		
+		// Only need to remove edges if something is actually selected
+		if (!graph.isSelectionEmpty()) {
+			
+			// Create array of all components in the selection
+			Object[] components = graph.getSelectionCells();
+			components = graph.getDescendants(components);
+			
+			// If any of the components are LEDs or seven segment displays, free them up for reuse
+			/*for (int i=0; i<components.length; i++) {
+				if (components[i] instanceof Ledr) {
+					Ledr ledr = (Ledr)components[i];
+					DeltaGraphModel model = (DeltaGraphModel)graph.getModel();
+					model.setLedUsed(ledr.getLedNumber(), ComponentPanel.LEDR, false);
+				}
+				else if (components[i] instanceof Ledg) {
+					Ledg ledg = (Ledg)components[i];
+					DeltaGraphModel model = (DeltaGraphModel)graph.getModel();
+					model.setLedUsed(ledg.getLedNumber(), ComponentPanel.LEDG, false);
+				}
+				else if (components[i] instanceof SevenSegment) {
+					SevenSegment sevenSegment = (SevenSegment)components[i];
+					DeltaGraphModel model = (DeltaGraphModel)graph.getModel();
+					model.setSevenSegmentUsed(sevenSegment.getSevenSegmentNumber(), false);
+				}
+			}*/
+			
+			// Create array of all edges connected to components in the selection
+			Set<Edge> edgeSet = DefaultGraphModel.getEdges(graph.getModel(), components);
+			Object[] edgeArray = edgeSet.toArray();
+			
+			// Pass the array of edges to the GraphModel for removal
+			graph.getModel().remove(edgeArray);
+		}
+		// END OF INSERT
 		action.actionPerformed(e);
 		// TODO: If an LED or seven segment display is cut (and not pasted back), make it available for reuse.
 	}
+	
 }

48843db24e0e6c6a23c4c4dbc136064e64a5d5aa
cwilso99@hotmail.com <cwilso99@hotmail.com@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-26 10:49:34 +0000

    Control points of edge now realign when a component it is attached to is moved. Note this only happens after the component is dropped - the edge still looks dodgy while you are dragging it.

--- trunk/src/org/delta/gui/diagram/DeltaGraphUI.java
+++ trunk/src/org/delta/gui/diagram/DeltaGraphUI.java
@@ -1,16 +1,25 @@
 package org.delta.gui.diagram;
 
 import java.awt.HeadlessException;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.io.Serializable;
+import java.util.Map;
+import java.util.Set;
 
-import org.jgraph.plaf.basic.BasicGraphUI;
+import org.jgraph.graph.AbstractCellView;
+import org.jgraph.graph.AttributeMap;
 import org.jgraph.graph.CellHandle;
 import org.jgraph.graph.CellView;
+import org.jgraph.graph.ConnectionSet;
 import org.jgraph.graph.EdgeView;
+import org.jgraph.graph.GraphConstants;
 import org.jgraph.graph.GraphContext;
+import org.jgraph.graph.GraphLayoutCache;
+import org.jgraph.graph.ParentMap;
+import org.jgraph.plaf.basic.BasicGraphUI;
 
 /**
  * Extension of BasicGraphUI that uses a custom RootHandle. This is so
@@ -136,6 +145,125 @@ public class DeltaGraphUI extends BasicGraphUI {
 						ignoreTargetGroup = graph.getGraphLayoutCache()
 								.getMapping(ignoreGroup, false);
 				}
+			}
+		}
+		
+		@Override
+		@SuppressWarnings("unchecked")
+		public void mouseReleased(MouseEvent event) {
+			try {
+				if (event != null && !event.isConsumed()) {
+					if (activeHandle != null) {
+						activeHandle.mouseReleased(event);
+						activeHandle = null;
+					} else if (isMoving && !event.getPoint().equals(start)) {
+						if (cachedBounds != null) {
+							
+							Point ep = event.getPoint();
+							Point2D point = new Point2D.Double(ep.getX()
+									- _mouseToViewDelta_x, ep.getY()
+									- _mouseToViewDelta_y);
+							Point2D snapCurrent = graph.snap(point);
+							
+							double dx = snapCurrent.getX() - start.getX();
+							double dy = snapCurrent.getY() - start.getY();
+							
+							if (!graph.isMoveBelowZero() && initialLocation.getX() + dx < 0)
+							    dx = -1 * initialLocation.getX();
+							if (!graph.isMoveBelowZero() && initialLocation.getY() + dy < 0)
+							    dy = -1 * initialLocation.getY();
+							
+							Point2D tmp = graph.fromScreen(new Point2D.Double(
+									dx, dy));
+							GraphLayoutCache.translateViews(views, tmp.getX(),
+									tmp.getY());
+						}
+						CellView[] all = graphLayoutCache
+								.getAllDescendants(views);
+						Map attributes = GraphConstants.createAttributes(all,
+								null);
+						if (event.isControlDown() && graph.isCloneable()) { // Clone
+							// Cells
+							Object[] cells = graph.getDescendants(graph
+									.order(context.getCells()));
+							// Include properties from hidden cells
+							Map hiddenMapping = graphLayoutCache
+									.getHiddenMapping();
+							for (int i = 0; i < cells.length; i++) {
+								Object witness = attributes.get(cells[i]);
+								if (witness == null) {
+									CellView view = (CellView) hiddenMapping
+											.get(cells[i]);
+									if (view != null
+											&& !graphModel.isPort(view
+													.getCell())) {
+										// TODO: Clone required? Same in
+										// GraphConstants.
+										AttributeMap attrs = (AttributeMap) view
+												.getAllAttributes().clone();
+										// Maybe translate?
+										// attrs.translate(dx, dy);
+										attributes.put(cells[i], attrs.clone());
+									}
+								}
+							}
+							ConnectionSet cs = ConnectionSet.create(graphModel,
+									cells, false);
+							ParentMap pm = ParentMap.create(graphModel, cells,
+									false, true);
+							cells = graphLayoutCache.insertClones(cells, graph
+									.cloneCells(cells), attributes, cs, pm, 0,
+									0);
+						} else if (graph.isMoveable()) { // Move Cells
+							ParentMap pm = null;
+
+							// Moves into group
+							if (targetGroup != null) {
+								pm = new ParentMap(context.getCells(),
+										targetGroup.getCell());
+							} else if (graph.isMoveOutOfGroups()
+									&& (ignoreTargetGroup != null && !ignoreTargetGroup
+											.getBounds().intersects(
+													AbstractCellView
+															.getBounds(views)))) {
+								pm = new ParentMap(context.getCells(), null);
+							}
+							graph.getGraphLayoutCache().edit(attributes,
+									disconnect, pm, null);
+						}
+						/* 
+						 * Realign points on edges around the components that have
+						 * been moved.
+						 */
+						// Find all the edges connected to the cells that have been moved
+						Set<DeltaEdge> edges = DeltaGraphModel.getEdges(graph.getModel(),
+								graph.getGraphLayoutCache().getCells(views));
+						int startPoint = 0;
+						// For each edge, find its view and call the realign points method
+						for (DeltaEdge edge : edges) {
+							DeltaEdgeView view = (DeltaEdgeView)graph.getGraphLayoutCache().
+								getMapping(edge,false);
+							for (int i=0; i<views.length; i++) {
+								if (views[i] == edge.getSource())
+									startPoint = 0;
+								else if (views[i] == edge.getTarget())
+									startPoint = view.getPointCount()-1;
+							}
+							view.realignPointsAround(startPoint, false, false);
+						}
+						event.consume();
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				ignoreTargetGroup = null;
+				targetGroup = null;
+				isDragging = false;
+				disconnect = null;
+				firstDrag = true;
+				current = null;
+				start = null;
 			}
 		}
 	}

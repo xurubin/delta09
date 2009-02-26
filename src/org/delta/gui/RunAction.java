f89af3be4965eb480033e38f1805105d91b8a38d
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-26 09:40:03 +0000

    Uncommenting run action code

--- src/org/delta/gui/RunAction.java
+++ src/org/delta/gui/RunAction.java
@@ -30,63 +30,63 @@ public class RunAction extends AbstractAction {
 	}
 
 	public void actionPerformed(ActionEvent e) {
-//		if (timesRun++ == 0) {
-//			MainWindow.get().setCursor(
-//					Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-//			/*
-//			 * program the board with JOP
-//			 */
-//			String blaster_type = "USB-Blaster";
-//			String quartus_path = "jop/quartus/altde2sram/jop.sof";
-//			String jop_command = "quartus_pgm -c " + blaster_type
-//					+ " -m JTAG -o p;" + quartus_path;
-//			try {
-//				Process p = Runtime.getRuntime().exec(jop_command);
-//
-//				BufferedReader input = new BufferedReader(
-//						new InputStreamReader(p.getInputStream()));
-//				String line;
-//				while ((line = input.readLine()) != null) {	
-//					System.err.println(line);
-//					if(line.contains("Error")) {
-//						throw new Exception("Error programming board.\nCheck that the USB blaster is connected and the board is switched on.");
-//					}
-//				}
-//				input.close();
-//				System.err.println("programmed board");
-//			} catch (Exception e1) {
-//				JOptionPane.showMessageDialog(MainWindow.get(),
-//						e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-//				MainWindow.get().setCursor(Cursor.getDefaultCursor());
-//				timesRun = 0;
-//				return;
-//			}
-//
-//			/*
-//			 * program the board with the daemon
-//			 */
-//
-//			String jop_runtime = "./jop/upload.exe jop/DE2_Daemon.jop";
-//			try {
-//				Process p = Runtime.getRuntime().exec(jop_runtime);
-//
-//				BufferedReader input = new BufferedReader(
-//						new InputStreamReader(p.getInputStream()));
-//				String line;
-//				while ((line = input.readLine()) != null) {
-//					System.err.println(line);
-//				}
-//				input.close();
-//				System.err.println("loaded JOP Daemon");
-//			} catch (Exception e2) {
-//				JOptionPane.showMessageDialog(MainWindow.get(),
-//						e2.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-//				MainWindow.get().setCursor(Cursor.getDefaultCursor());
-//				timesRun = 0;
-//				return;
-//			}
-//			MainWindow.get().setCursor(Cursor.getDefaultCursor());
-//		}
+		if (timesRun++ == 0) {
+			MainWindow.get().setCursor(
+					Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+			/*
+			 * program the board with JOP
+			 */
+			String blaster_type = "USB-Blaster";
+			String quartus_path = "jop/quartus/altde2sram/jop.sof";
+			String jop_command = "quartus_pgm -c " + blaster_type
+					+ " -m JTAG -o p;" + quartus_path;
+			try {
+				Process p = Runtime.getRuntime().exec(jop_command);
+
+				BufferedReader input = new BufferedReader(
+						new InputStreamReader(p.getInputStream()));
+				String line;
+				while ((line = input.readLine()) != null) {	
+					System.err.println(line);
+					if(line.contains("Error")) {
+						throw new Exception("Error programming board.\nCheck that the USB blaster is connected and the board is switched on.");
+					}
+				}
+				input.close();
+				System.err.println("programmed board");
+			} catch (Exception e1) {
+				JOptionPane.showMessageDialog(MainWindow.get(),
+						e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				MainWindow.get().setCursor(Cursor.getDefaultCursor());
+				timesRun = 0;
+				return;
+			}
+
+			/*
+			 * program the board with the daemon
+			 */
+
+			String jop_runtime = "./jop/upload.exe jop/DE2_Daemon.jop";
+			try {
+				Process p = Runtime.getRuntime().exec(jop_runtime);
+
+				BufferedReader input = new BufferedReader(
+						new InputStreamReader(p.getInputStream()));
+				String line;
+				while ((line = input.readLine()) != null) {
+					System.err.println(line);
+				}
+				input.close();
+				System.err.println("loaded JOP Daemon");
+			} catch (Exception e2) {
+				JOptionPane.showMessageDialog(MainWindow.get(),
+						e2.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				MainWindow.get().setCursor(Cursor.getDefaultCursor());
+				timesRun = 0;
+				return;
+			}
+			MainWindow.get().setCursor(Cursor.getDefaultCursor());
+		}
 		// start simulation
 		MainWindow.get().stop_action.setEnabled(true);
 		MainWindow.get().run_action.setEnabled(false);

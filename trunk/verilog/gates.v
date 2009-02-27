d328b5bf3190488a88e08a7cc5183894de0eca78
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-27 10:19:26 +0000

    Updating verilog translation so that LEDs aren't inverted

--- trunk/verilog/gates.v
+++ trunk/verilog/gates.v
@@ -14,12 +14,12 @@ module rslatch(output q1, output q2, input s, input r);
 	nor(q2, s, q1);
 endmodule
 
-module hexdisplay(output [6:0] h, input w0, input w1, input w2, input w3, input w4, input w5, input w6);
-	assign hex[0] = w0;
-	assign hex[1] = w1;
-	assign hex[2] = w2;
-	assign hex[3] = w3;
-	assign hex[4] = w6;
-	assign hex[5] = w4;
-	assign hex[6] = w5;	
+module hexdisplay(output [6:0] hex, input w0, input w1, input w2, input w3, input w4, input w5, input w6);
+	assign hex[0] = ~w0;
+	assign hex[1] = ~w1;
+	assign hex[2] = ~w2;
+	assign hex[3] = ~w3;
+	assign hex[4] = ~w6;
+	assign hex[5] = ~w4;
+	assign hex[6] = ~w5;
 endmodule

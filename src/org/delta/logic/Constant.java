48aa8a6c8cac6883ff7055511f13b553e15a262c
justus.matthiesen <justus.matthiesen@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-27 22:16:44 +0000

    Implements/overwrites the equal method.

--- src/org/delta/logic/Constant.java
+++ src/org/delta/logic/Constant.java
@@ -28,4 +28,24 @@ public class Constant implements Formula, Serializable {
         return state;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((state == null) ? 0 : state.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Constant other = (Constant) obj;
+        if (state == null) {
+            if (other.state != null) return false;
+        } else if (!state.equals(other.state)) return false;
+        return true;
+    }
+
 }

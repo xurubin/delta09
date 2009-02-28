836b679fe08f5a8467fb5c8a173ea0b00123b4ba
RobertAlanDuncan <RobertAlanDuncan@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-28 15:31:35 +0000

    Modified verilog translation for ROM and RAM.

--- src/org/delta/circuit/component/RomComponent.java
+++ src/org/delta/circuit/component/RomComponent.java
@@ -2,27 +2,32 @@ package org.delta.circuit.component;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Set;
 
 import org.delta.circuit.Component;
 import org.delta.circuit.ComponentGraph;
-import org.delta.circuit.Gate;
-import org.delta.circuit.gate.GateFactory;
-import org.delta.circuit.gate.MemoryFunction;
-import org.delta.logic.And;
+import org.delta.circuit.ComponentWire;
 
 public class RomComponent extends Component {
 
-    public RomComponent(int selectCount, int outputCount,
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<Integer> presetValue;
+
+	public RomComponent(int selectCount, int outputCount,
             List<Integer> store) {
         super(selectCount, outputCount);
+        this.presetValue = store;
         
         ComponentGraph graph = new ComponentGraph(false);
         
         RamComponent rom = new RamComponent(selectCount, outputCount);
         graph.addVertex(rom);
-        
+
         rom.preset(store);
         
         // Set inputs.
@@ -43,5 +48,34 @@ public class RomComponent extends Component {
         
         fromComponentGraph(graph, inputs, outputs);
     }
+    
+    @Override
+	public String getVerilogMethod(String name, LinkedHashMap<ComponentWire, String> inputWires,
+			LinkedHashMap<ComponentWire, String> outputWires) {
+
+		ComponentWire[] output1 = new ComponentWire[this.getOutputWires(0).size()];
+		this.getOutputWires(0).toArray(output1);
+
+		String mainOutput1 = output1.length > 0 ? outputWires.get(output1[0]) : "";
+		
+		String data = "assign " + name + "_data_wire  = 16'b";
+		for(int i = 0; i < 16; i++) {
+			data += (this.presetValue.get(i) == 0 ? "0" : "1");
+		}
+		data += ";\n";
+		String result = "rom16 " + name + "(" + mainOutput1 + ", "
+				+ inputWires.get(this.getInputWire(0)) + ", "
+				+ inputWires.get(this.getInputWire(1))  + ", "
+				+ inputWires.get(this.getInputWire(2))  + ", "
+				+ inputWires.get(this.getInputWire(3)) + ", "
+				+ name + "_data_wire" + 
+				", world_clock);";
+
+		for (int i = 1; i < output1.length; i++) {
+			result += "\nassign " + outputWires.get(output1[i]) + " = " + mainOutput1;
+		}
+
+		return data + result;
+	}
 
 }

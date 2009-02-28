96747d73ac2eb348e9cd089237350f44d2beacfb
justus.matthiesen <justus.matthiesen@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-02-28 00:41:47 +0000

    Implements test for equal method in class Constant.

--- test/org/delta/logic/ConstantTest.java
+++ test/org/delta/logic/ConstantTest.java
@@ -35,5 +35,29 @@ public class ConstantTest {
         Constant constantS0Wire = new Constant(s0);
         assertSame(s0, constantS0Wire.evaluate());
     }
+    
+    @Test
+    public final void equalsTest() {
+        Constant constantS0a = new Constant(State.S0);
+        Constant constantS0b = new Constant(State.S0);
+        assertTrue(constantS0a.equals(constantS0b));
+        
+        Constant constantS1a = new Constant(State.S1);
+        Constant constantS1b = new Constant(State.S1);
+        assertTrue(constantS1a.equals(constantS1b));
+        
+        Constant constantSXa = new Constant(State.SX);
+        Constant constantSXb = new Constant(State.SX);
+        assertTrue(constantSXa.equals(constantSXb));
+        
+        assertFalse(constantS0a.equals(constantS1a));
+        assertFalse(constantS1a.equals(constantS0a));
+        
+        assertFalse(constantS0a.equals(constantSXa));
+        assertFalse(constantSXa.equals(constantS0a));
+        
+        assertFalse(constantS1a.equals(constantSXa));
+        assertFalse(constantSXa.equals(constantS1a));
+    }
 
 }

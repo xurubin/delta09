eef7f6ce5e9d0adf2587f8d6b0f026a1d6e8e7bd
davidjweston <davidjweston@97b6f368-e324-11dd-a893-8d7ce9fbef62>
2009-03-01 01:47:17 +0000

    MemoryDialog now works (user can select initial ROM values)

--- trunk/src/org/delta/gui/diagram/MemoryDialog.java
+++ trunk/src/org/delta/gui/diagram/MemoryDialog.java
@@ -1,16 +1,22 @@
 package org.delta.gui.diagram;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.BorderFactory;
 import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
@@ -22,55 +28,116 @@ public class MemoryDialog extends JDialog {
 	/**
 	 * 
 	 */
-	private List<Integer> storeValues;
-	public static final int WIDTH = 500;
-	public static final int HEIGHT = 300;
 	
 	private static final long serialVersionUID = 1L;
 	
-	public MemoryDialog(Frame owner, String title) {
-		super(owner, title);
-		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	private Object [][] data;
+	
+	public MemoryDialog (Frame owner, String title)
+	{
+		super (owner, title);
 		
-		Container cp = this.getContentPane();
+		Container cp = getContentPane();
+		
 		cp.setLayout ( new GridLayout (0, 1) );
-		cp.add ( new JLabel (MainWindow.getTranslatorString("ROM_CONTENTS"), JLabel.CENTER) );
 		
-		JComboBox combo = new JComboBox ( new String [] {"0","1"} );
+		JPanel panel3 = new JPanel();
+		panel3.setBorder ( BorderFactory.createEmptyBorder (5, 10, 0, 10) );
 		
-//		Object [][] data = { {1,1,1,1},{0,0,0,0},{1,1,0,0},{0,0,1,1}};
-//		String [] column_names = {"Your","face","is","silly"};
+		panel3.add ( new JLabel (MainWindow.getTranslatorString ("ROM_CONTENTS"), JLabel.CENTER) );
 		
-		TableModel dataModel = new AbstractTableModel() {
+		cp.add (panel3);
+		
+		JComboBox combo = new JComboBox ( new Integer [] { new Integer (0), new Integer (1) } );
+		
+		Object [][] d = { { new Integer (0), new Integer (0), new Integer (0), new Integer (0) },
+				  		  { new Integer (0), new Integer (0), new Integer (0), new Integer (0) },
+				  		  { new Integer (0), new Integer (0), new Integer (0), new Integer (0) },
+				  		  { new Integer (0), new Integer (0), new Integer (0), new Integer (0) } };
+		data = d;
+		
+		TableModel dataModel = new AbstractTableModel()
+		{
 			private static final long serialVersionUID = 1L;
-			public int getColumnCount() { return 4; }
-			public int getRowCount() { return 4; }
-			public Object getValueAt (int row, int col) { return new Integer (row * col); }
+			
+			public int getColumnCount()
+			{
+				return 4;
+			}
+			
+			public int getRowCount()
+			{
+				return 4;
+			}
+			
+			public Object getValueAt (int row, int col)
+			{
+				return data [row][col];
+			}
+			
+			public void setValueAt (Object value, int row, int col)
+			{
+				data [row][col] = value;
+				fireTableCellUpdated (row, col);
+			}
+			
+			public boolean isCellEditable (int row, int col)
+			{
+				return true;
+			}
 		};
 		
 		JTable table = new JTable (dataModel);
 		
 		for (int i = 0; i < 4; i++)
-			table.getColumnModel().getColumn(i).setWidth (10);
-
-		table.setCellEditor ( new DefaultCellEditor (combo) );
+			table.getColumnModel().getColumn(i).setCellEditor ( new DefaultCellEditor (combo) );
 		
-//		cp.add ( table.getTableHeader() );
-		cp.add (table);
+		table.setRowSelectionAllowed    (false);
+		table.setColumnSelectionAllowed (false);
+		table.setCellSelectionEnabled   (false);
 		
-		storeValues = new ArrayList <Integer> (16);
-		for (int i = 0; i < 16; i++)
-			storeValues.add ( new Integer (1) );
+		table.setBorder ( new javax.swing.border.LineBorder (Color.BLACK) );
+		table.setPreferredSize ( new Dimension (160, table.getPreferredSize().height) );
+		
+		JPanel panel = new JPanel();
+		
+		panel.setBorder ( BorderFactory.createEmptyBorder (0, 25, 15, 25) );
+		panel.add (table);
+		
+		cp.add (panel);
+		
+		JButton button = new JButton ( MainWindow.getTranslatorString ("OK") );
+		
+		button.addActionListener ( new ActionListener() {
+			public void actionPerformed (ActionEvent arg0) {
+				dispose();
+			}
+		} );
+		
+		JPanel panel2 = new JPanel();
+		
+		panel2.setBorder ( BorderFactory.createEmptyBorder (0, 50, 0, 50) );
+		panel2.add (button);
+		
+		cp.add (panel2);
 	}
 	
-	public int showDialog() {
-		this.setModal(true);
-		this.pack();
-		super.setVisible(true);
+	public int showDialog()
+	{
+		setModal (true);
+		pack();
+		super.setVisible (true);
 		return 0;
 	}
 	
-	public List<Integer> getStoreValues() {
+	public List<Integer> getStoreValues()
+	{
+		List <Integer> storeValues = new ArrayList <Integer> (16);
+		
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+				storeValues.add ( (Integer) data [j][i] );
+		
 		return storeValues;
 	}
 	
